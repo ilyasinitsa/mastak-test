110df3e0118f6f89cab84577bdc87f5c5f74901e
ilya.sinitsa <ilya.sinitsa@yandex.ru>
2023-06-28 23:19:54 +0300

    created slider

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.prmix-ebook-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '<img class="prev" src="img/prev.svg">',
+        nextArrow: '<img class="next" src="img/next.svg">'
+    });
+});
