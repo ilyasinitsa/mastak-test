7902c9d678f717a3b809f80dc040e0ba5fa6894c
ilya.sinitsa <ilya.sinitsa@yandex.ru>
2023-06-29 17:51:51 +0300

    added adapted layout and animations

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ $(document).ready(function () {
         slidesToScroll: 1,
         arrows: true,
         prevArrow: '<img class="prev" src="img/prev.svg">',
-        nextArrow: '<img class="next" src="img/next.svg">'
+        nextArrow: '<img class="next" src="img/next.svg">',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 });
